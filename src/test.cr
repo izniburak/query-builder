8658346398f6c86b6f9b384f4608dfa04f0a03fb
İzni Burak Demirtaş <izniburak@outlook.com>
2016-08-10 03:39:10 +0300

    update example codes.

--- src/test.cr
+++ src/test.cr
@@ -1,5 +1,7 @@
 require "./query-builder"
 
-query = Query::Builder.new
+builder = Query::Builder.new
 
-p query.table("test").where("id", 17).or_where("language", "crystal").get
+p builder.table("test").where("id", 17).or_where("language", "crystal").get
+
+p builder.query("SELECT id, title FROM test_table WHERE id = ? AND title = ? ORDER BY id DESC LIMIT 10", [17, "Crystal"])
