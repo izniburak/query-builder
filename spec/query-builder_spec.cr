1b597fc51467f574facf6ec0f02284faa53d6407
İzni Burak Demirtaş <izniburak@outlook.com>
2016-08-16 19:16:45 +0300

    added JOIN method specs.

--- spec/query-builder_spec.cr
+++ spec/query-builder_spec.cr
@@ -18,22 +18,16 @@ describe Query::Builder do
     query.should eq "SELECT id, title, content, status FROM test"
   end
 
+  it "sql join" do
+    builder = Query::Builder.new
+    query = builder.table("test").left_join("foo", "test.id", "foo.page_id").get_all
+    query.should eq "SELECT * FROM test LEFT JOIN foo ON test.id = foo.page_id"
+  end
+
   it "where and or_where" do
     builder = Query::Builder.new
     query = builder.table("test").where("auth", 1).or_where("auth", 2).get_all
     query.should eq "SELECT * FROM test WHERE auth = '1' OR auth = '2'"
-  end
-
-  it "sql limit" do
-    builder = Query::Builder.new
-    query = builder.table("test").where("status", 1).limit(10, 20).get_all
-    query.should eq "SELECT * FROM test WHERE status = '1' LIMIT 10, 20"
-  end
-
-  it "sql order by" do
-    builder = Query::Builder.new
-    query = builder.table("test").where("active", 1).order_by("id", "desc").limit(5).get_all
-    query.should eq "SELECT * FROM test WHERE active = '1' ORDER BY id DESC LIMIT 5"
   end
 
   it "sql where in" do
@@ -46,5 +40,17 @@ describe Query::Builder do
     builder = Query::Builder.new
     query = builder.table("test").where("status", 1).between("age", 18, 30).get_all
     query.should eq "SELECT * FROM test WHERE status = '1' AND age BETWEEN '18' AND '30'"
+  end
+
+  it "sql limit" do
+    builder = Query::Builder.new
+    query = builder.table("test").where("status", 1).limit(10, 20).get_all
+    query.should eq "SELECT * FROM test WHERE status = '1' LIMIT 10, 20"
+  end
+
+  it "sql order by" do
+    builder = Query::Builder.new
+    query = builder.table("test").where("active", 1).order_by("id", "desc").limit(5).get_all
+    query.should eq "SELECT * FROM test WHERE active = '1' ORDER BY id DESC LIMIT 5"
   end
 end
